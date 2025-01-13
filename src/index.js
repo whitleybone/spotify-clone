32a9c909534bf6172c7e74328d9841d94d0c3efd
Whitley Bone <whitley.bone@alertmedia.com>
2025-01-13 10:39:11 -0600

    add basename index js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Import the correct method for React 18
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-// Create the root and render the app
+// Adjust the `basename` for GitHub Pages
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/spotify-clone"> {/* Update the basename */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
