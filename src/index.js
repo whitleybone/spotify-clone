d415fa50ff6a52631bccc2ad2bc4802c38a5d5d1
Whitley Bone <whitley.bone@alertmedia.com>
2025-01-13 11:44:13 -0600

    revert back to original route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/spotify-clone"> {/* Update the basename */}
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>
